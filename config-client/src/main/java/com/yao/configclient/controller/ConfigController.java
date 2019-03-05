eafa867de3966a80d0755d876e6321c64b0b6a4f
yys1892 <YS.yaoxiang@h3c.com>
2019-03-05 15:46:07 +0800

    新增config模块

---
+++ config-client/src/main/java/com/yao/configclient/controller/ConfigController.java
@@ -0,0 +1,21 @@
+package com.yao.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: ys1892
+ * @Date: 2019-03-05
+ * @Time: 15:23
+ */
+@RestController
+public class ConfigController {
+    @Value("${address}")
+    private String address;
+    @GetMapping("/getConfig")
+    public String getConfig(){
+        return "get address="+address+" from git ";
+    }
+}
